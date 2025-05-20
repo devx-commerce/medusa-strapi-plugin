9c8d0ae36952883c5645552ac004752be8eb4b61
Brijesh Patadiya <brijesh.patadiya@devxconsultancy.com>
2025-05-20 15:26:04 +0530

    dummy commit

--- src/workflows/create-products-strapi.ts
+++ src/workflows/create-products-strapi.ts
@@ -1,30 +1,28 @@
 import { ProductDTO } from "@medusajs/framework/types";
 import {
-  createWorkflow,
-  WorkflowResponse,
+    createWorkflow,
+    WorkflowResponse,
 } from "@medusajs/framework/workflows-sdk";
 import { useQueryGraphStep } from "@medusajs/medusa/core-flows";
 import { createProductsStrapiStep } from "./steps/create-products-strapi";
 
 type WorkflowInput = {
-  product_ids: string[];
+    product_ids: string[];
 };
 
 export const createProductsStrapiWorkflow = createWorkflow(
-  { name: "create-products-strapi-workflow" },
-  (input: WorkflowInput) => {
-    const { data } = useQueryGraphStep({
-      entity: "product",
-      fields: ["id", "title", "type", "status", "handle", "variants.*"],
-      filters: {
-        id: input.product_ids,
-      },
-    });
+    { name: "create-products-strapi-workflow" },
+    (input: WorkflowInput) => {
+        const { data } = useQueryGraphStep({
+            entity: "product",
+            fields: ["id", "title", "type", "status", "handle", "variants.*"],
+            filters: { id: input.product_ids },
+        });
 
-    const strapiProducts = createProductsStrapiStep({
-      products: data as ProductDTO[],
-    });
+        const strapiProducts = createProductsStrapiStep({
+            products: data as ProductDTO[],
+        });
 
-    return new WorkflowResponse(strapiProducts);
-  },
+        return new WorkflowResponse(strapiProducts);
+    }
 );
